e5f52618398f9684523713f6471f17da460649f2
Arpit Mittal <arpit@roofpik.com>
2016-08-03 12:33:11 +0530

    app start updated

--- www/js/app/app-start.ctrl.js
+++ www/js/app/app-start.ctrl.js
@@ -1,86 +1,13 @@
-angular.module('starter.controllers', [])
+app.controller('appStatusCtrl', function($scope, $timeout, $ionicLoading, $state, $cordovaDevice, $cordovaNetwork, $ionicPopup, $rootScope) {
 
-app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-app.controller('PlaylistsCtrl1', function($scope) {
-
-
-  // var test = {
-  //   'a': 'arpit'
-  // }
-
-  //  // Get a key for a new Post.
-  // var newPostKey = db.ref().child('test').push().key;
-  // var updates = {};
-  // updates['/test/' + newPostKey] = test;
-  // db.ref().update(updates);
-})
-
-// app.controller('PlaylistCtrl', function($scope, $stateParams) {
-
-
-// });
-
-
-
-app.controller('PlaylistsCtrl', function($scope, $timeout, $ionicLoading, $state, $cordovaDevice, $cordovaNetwork, $ionicPopup, $rootScope) {
-      $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-
-    localStorage.clear();
-    console.log("cleared Local Storage");
     $ionicLoading.show();
     var appInfo = {};
     var location = {};
     checkAppInfo();
-    
+
     function checkAppInfo() {
         var hasAppInfo = checkLocalStorage("appInfo");
+        alert("hasAppInfo", hasAppInfo);
         if (!hasAppInfo) {
             initialiseAppInfo();
             initialiseLocation();
@@ -89,13 +16,11 @@ app.controller('PlaylistsCtrl', function($scope, $timeout, $ionicLoading, $state
     }
 
     function checkAppStatus() {
-      console.log("checkAppStatus");
         var checkNewUser = checkLocalStorage('appStatus');
         if (checkNewUser) {
             db.ref('appStatus').once('value', function(snapshot) {
                 var newStatus = snapshot.val();
                 var currentStatus = JSON.parse(window.localStorage['appStatus']);
-                window.localStorage['appStatus'] = JSON.stringify(snapshot.val())
                 if (newStatus.live == true) {
                     if (newStatus.version > currentStatus.appversion) {
                         $ionicLoading.hide();
@@ -108,7 +33,7 @@ app.controller('PlaylistsCtrl', function($scope, $timeout, $ionicLoading, $state
                         if (newStatus.nearbyVersion > currentStatus.nearbyVersion) {
                             updateNearbyData();
                         }
-                        if (newStatus.projectVersion > currentStatus.projectVersion) {
+                        if (newStatus.nearbyVersion > currentStatus.nearbyVersion) {
                             updateProjectData();
                         }
                         checkLoginStatus();
@@ -119,9 +44,6 @@ app.controller('PlaylistsCtrl', function($scope, $timeout, $ionicLoading, $state
                 }
             });
         } else {
-          console.log("checkAppStatus Else");
-          db.ref('appStatus').once('value', function(snapshot) {
-                var newStatus = snapshot.val();
             if (newStatus.live == false) {
                 $ionicLoading.hide();
                 //$state.go('underconstruction');
@@ -130,13 +52,11 @@ app.controller('PlaylistsCtrl', function($scope, $timeout, $ionicLoading, $state
                     window.localStorage['appStatus'] = JSON.stringify(snapshot.val())
                 })
                 updateLocationData();
-                console.log()
                 updateNearbyData();
                 updateProjectData();
                 $ionicLoading.hide();
-                
+                //$state.go('intro-slider');
             }
-          });
         }
     }
 
@@ -182,8 +102,8 @@ app.controller('PlaylistsCtrl', function($scope, $timeout, $ionicLoading, $state
         var date = new Date();
         var currTimeStamp = date.getTime();
         appInfo = {
-            udid: '',
-            uuid: currTimeStamp,
+            udid: 'na',
+            uuid: 'na',
             os: '',
             platform: '',
             version: '',
@@ -206,15 +126,12 @@ app.controller('PlaylistsCtrl', function($scope, $timeout, $ionicLoading, $state
             appInfo.version = deviceInformation.version;
             appInfo.model = deviceInformation.model;
             appInfo.manufacture = deviceInformation.manufacturer;
-            db.ref('deviceInformation/' + appInfo.uuid).update(appInfo).then(function() {});
+            appInfo. = timeStamp;
         } catch (e) {
             appInfo.error = e;
-            var newPostKey = db.ref().child('deviceInformation').push().key;
-            db.ref('deviceInformation/notRegistered/' + newPostKey).update(appInfo).then(function() {});
         };
         window.localStorage['appInfo'] = JSON.stringify(appInfo);
-        
-        console.log("Registered Device");
+        db.ref('deviceInformation/' + appInfo.uuid).update(appInfo).then(function() {});
     }
 
     function initialiseLocation() {
@@ -231,8 +148,6 @@ app.controller('PlaylistsCtrl', function($scope, $timeout, $ionicLoading, $state
             zoneName: "Sohna Road"
         }
         window.localStorage['selectedLocation'] = JSON.stringify(location);
-        console.log("Location Initialised");
     }
 
 });
-
